0a6ada556205b27b40173a9aaacf19824acfe6f6
StasonLV <talemane@gmail.com>
2022-02-13 17:59:38 +0300

    Initial Commit

---
+++ WeatherTrainingApp(SwiftUI)/WeatherTrainingApp_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrainingApp_SwiftUI_App.swift
+//  WeatherTrainingApp(SwiftUI)
+//
+//  Created by Кристина on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrainingApp_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
